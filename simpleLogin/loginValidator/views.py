0677fff1826059e798d17ba31999ff0351318fad
Gopal Gautam <entrin_2gworld@yahoo.com>
2015-09-11 13:05:02 +0545

    Fixed a bug for multiple data row in csv file

--- simpleLogin/loginValidator/views.py
+++ simpleLogin/loginValidator/views.py
@@ -11,16 +11,18 @@ def get_login(request):
 			last_name = form.cleaned_data["lastName"]
 			phone = form.cleaned_data["phone"]
 			address = form.cleaned_data["address"]
+			authenticated = False
 			with open("/home/gopal/env/simple_login/simpleLogin/data.csv", "rb") as csvfile:
 				datareader = csv.reader(csvfile, delimiter=",")
 				for datalist in datareader:
 					row = [item.strip().lower() for item in datalist]
 					if first_name.lower() in row and last_name.lower() in row and phone.lower() in row and address.lower() in row:
+						authenticated = True
 						message = "Welcome to the site"
 						return render(request, "welcome.html", {"message":message})	
-					else:
-						message = "Sorry we dont recognize you"
-						return render(request, "login.html", {"message":message, "form":form})
+			if not authenticated:
+				message = "Sorry we dont recognize you"
+				return render(request, "login.html", {"message":message, "form":form})
 	else:
 		form = LoginForm()
 
